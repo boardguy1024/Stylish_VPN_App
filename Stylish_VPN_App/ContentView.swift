f37166f562b75a243e42608aa78cbe168fefb806
park kyung suk <boardguy1024@gmail.com>
2021-01-12 16:48:41 +0900

    Initial Commit

---
+++ Stylish_VPN_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stylish_VPN_App
+//
+//  Created by park kyung suk on 2021/01/12.
+//  Copyright © 2021 park kyung suk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
